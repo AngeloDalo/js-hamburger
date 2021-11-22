4440b5feadaaeea152c9b8c19abce6200c0685be
AngeloDalo <angelo251158@gmail.com>
2021-11-22 14:15:55 +0100

    first click

---
+++ main.js
@@ -0,0 +1,8 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu'); 
+const hamburgerButton = document.querySelector('.fa-bars'); 
+
+hamburgerButton.addEventListener ( 'click', function() {
+    const hamburgerMenu = document.querySelector('.hamburger-menu'); 
+    hamburgerMenu.style.display = "block"
+});
+   
