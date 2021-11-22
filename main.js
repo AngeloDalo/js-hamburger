7f01bf794219e5dc6e6318c3c7e0331cd7170d5f
AngeloDalo <angelo251158@gmail.com>
2021-11-22 14:18:39 +0100

    second click

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu'); 
 const hamburgerButton = document.querySelector('.fa-bars'); 
+const crossButton = document.querySelector('.fa-times'); 
 
 hamburgerButton.addEventListener ( 'click', function() {
-    const hamburgerMenu = document.querySelector('.hamburger-menu'); 
     hamburgerMenu.style.display = "block"
+});
+
+crossButton.addEventListener ( 'click', function() {
+    hamburgerMenu.style.display = "none"
 });
    
