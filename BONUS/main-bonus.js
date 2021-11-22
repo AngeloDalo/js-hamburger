e9c2a44ea1f8501c77deffabeb5ae706894549c2
AngeloDalo <angelo251158@gmail.com>
2021-11-22 14:58:40 +0100

    bonus

--- BONUS/main-bonus.js
+++ BONUS/main-bonus.js
@@ -1,9 +1,15 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu'); 
 const hamburgerButton = document.querySelector('.fa-bars'); 
 const crossButton = document.querySelector('.fa-times'); 
+const list = document.querySelector('ul'); 
+
+list.addEventListener ( 'click', function() {
+    list.style.backgroundColor = "red";
+});
 
 hamburgerButton.addEventListener ( 'click', function() {
     hamburgerMenu.style.display = "block";
+    hamburgerMenu.style.background ="green";
 });
 
 crossButton.addEventListener ( 'click', function() {
